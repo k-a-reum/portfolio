b292bb2f5918eb5a1644ff452652b7304563352a
k-a-reum <kimareum298@gmail.com>
2022-05-20 21:41:15 +0900

    JS Contact Button

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ document.addEventListener('scroll', ()=>{
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
-    console.log(event.target.dataset.link);
     const target=event.target;
     const link=target.dataset.link;
     if(link==null){
@@ -23,3 +22,12 @@ navbarMenu.addEventListener('click', (event)=>{
     const scrollTo=document.querySelector(link);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 });
+
+// Handle scrolling when tapping on the contact button
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', (event)=>{
+    const target=event.target;
+    const link=target.dataset.link;
+    const scrollTo=document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+})
