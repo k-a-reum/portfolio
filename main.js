fe4a55f299848dd880363fc8d780309498a1c6f4
k-a-reum <kimareum298@gmail.com>
2022-05-23 21:17:28 +0900

    Select navitem

--- main.js
+++ main.js
@@ -20,8 +20,7 @@ navbarMenu.addEventListener('click', (event)=>{
         return;
     }
     navbarMenu.classList.remove('open');
-    const scrollTo=document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
 
 // Navbar toggle button for small screen
@@ -33,8 +32,7 @@ navbarToggleBtn.addEventListener('click',()=>{
 // Handle scrolling when tapping on the contact button
 const contactBtn = document.querySelector('.home__contact');
 contactBtn.addEventListener('click', ()=>{
-    const scrollTo=document.querySelector('#contact');
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView('#contact');
 });
 
 // Make home slowly fade to transparent as the window scrolls down
@@ -57,8 +55,7 @@ document.addEventListener('scroll', ()=>{
 
 // Handle click on the "arrow up" button
 arrowUp.addEventListener('click', ()=>{
-    const scrollTo=document.querySelector('#home');
-    scrollTo.scrollIntoView({behavior:'smooth'});
+    scrollIntoView('#home');
 });
 
 // Projects
@@ -90,3 +87,70 @@ workBtnContainer.addEventListener('click', (e)=>{
         projectContainer.classList.remove('anim-out');
     },300);
 });
+
+// 1. 모든 섹션 요소들과 메뉴아이템을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact',
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+    document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+        console.log('y');
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        // 스크롤링이 아래로 되어서 페이지가 올라옴
+        if (entry.boundingClientRect.y < 0) {
+            selectedNavIndex = index + 1;
+        } else {
+            selectedNavIndex = index - 1;
+        }
+    }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (
+        Math.round(window.scrollY + window.innerHeight) >=
+        document.body.clientHeight
+    ) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
+
+//ScrollIntoView
+function scrollIntoView(selector){
+    const scrollTo=document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
